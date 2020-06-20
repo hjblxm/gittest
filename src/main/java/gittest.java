3bfad2dc2415fea6cc3071a3b6aa80d588f48a4b
hjblxm <58283214+hjblxm@users.noreply.github.com>
2020-06-21 00:57:43 +0800

    Update gittest.java

--- src/main/java/gittest.java
+++ src/main/java/gittest.java
@@ -2,5 +2,6 @@ public class gittest {
     public static void main(String[] args) {
         System.out.println("创建git仓库");
         System.out.println("网上创建看能不能直接获取");
+        System.out.println("网上再次创建看能不能直接获取");
     }
 }
